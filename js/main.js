3b1a13211c0b8ba77eabf7a97a3fc54085a40763
Deepak Patra <deepak@itobuz.com>
2023-03-24 19:03:06 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+// import Swiper, { Navigation, Pagination } from 'swiper';
+// // import Swiper and modules styles
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
+
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
